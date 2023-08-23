6596a01e52a60291c6ec23fefffdebfd65feffe1
Jenaide <jenaidesibolie@gmail.com>
2023-08-23 19:33:03 +0000

    bug fix

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -35,7 +35,4 @@ class FIFOCache(BaseCaching):
         """
         method that retrieves an item by key
         """
-        if key is None or key not in self.cache_data:
-            return None
-
-        return self.cache_data.get[key]
+        return self.cache_data.get(key, None)
