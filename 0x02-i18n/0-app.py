8a082e79be8d3fe637cd322839afa58c861ad833
Jenaide <jenaidesibolie@gmail.com>
2023-08-29 19:16:26 +0000

    bug fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,8 +4,12 @@ Created by Jenaide Sibolie
 """
 from flask import Flask, render_template
 
-
-@app.route('/')
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
 def index:
     """ route that outputs hello world """
-    return render_template("index.html")
+    return render_template("0-index.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
