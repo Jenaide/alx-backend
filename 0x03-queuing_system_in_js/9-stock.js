63b5422184dcfb24562f6aa011bb3c0960c82c6f
Jenaide <jenaidesibolie@gmail.com>
2023-09-27 20:50:38 +0000

    completed stock js

---
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+import express from 'express';
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+const listProducts = [
+  {
+    itemId: 1,
+    itemName: 'Suitcase 250',
+    price: 50,
+    initialAvailableQuantity: 4
+  },
+  {
+    itemId: 2,
+    itemName: 'Suitcase 450',
+    price: 100,
+    initialAvailableQuantity: 10
+  },
+  {
+    itemId: 3,
+    itemName: 'Suitcase 650',
+    price: 350,
+    initialAvailableQuantity: 2
+  },
+  {
+    itemId: 4,
+    itemName: 'Suitcase 1050',
+    price: 550,
+    initialAvailableQuantity: 5
+  },
+];
+
+const getItemById = (id) => {
+  const items = listProducts.find(obj => obj.itemId === id);
+  if (items) {
+    return Object.fromEntries(Object.entries(items));
+  }
+};
+
+const app = express();
+const client = createClient();
+const PORT = 1245;
+
+const getCurrentReservedStockById = async (itemId) => {
+  return promisify(client.GET).bind(client)(`item.${itemId}`);
+};
+
+app.get('/list_products/:itemId(\\d+)', (req, res) => {
+  const itemId = Number.parseInt(req.params.itemId);
+  const productItem = getItemById(Number.parseInt(itemId));
+  if (!productItem) {
+    res.json({ status: 'Product not found' });
+  }
+
+  getCurrentReservedStockById(itemId)
+    .then((result) => Number.parseInt(result || 0))
+    .then((reservedStock) => {
+      productItem.currentQuantity = productItem.initialAvailableQuantity - reservedStock;
+      res.json(productItem);
+    });
+});
+
+app.get('/reserve_product/:itemId', (req, res) => {
+  const itemId = Number.parseInt(req.params.itemId);
+  const productItem = getItemById(Number.parseInt(itemId));
+
+  if (!productItem) {
+    res.json({ status: 'Product not found' });
+    return;
+  }
+  getCurrentReservedStockById(itemId)
+    .then((result) => Number.parseInt(result || 0))
+    .then((reservedStock) => {
+      if (reservedStock >= productItem.initialAvailableQuantity) {
+        res.json({ status: 'Not enough stock available', itemId });
+        return;
+      }
+      reserveStockById(itemId, reservedStock + 1)
+        .then(() => {
+          res.json({ status: 'Reservation confirmed', itemId });
+        });
+    });
+});
+
+const resetProductsStock = () => {
+  return Promise.all(
+    listProducts.map(
+      item => promisify(client.SET).bind(client)(`item.${item.itemId}`, 0),
+    )
+  );
+};
+
+app.listen(PORT, () => {
+  resetProductsStock()
+    .then(() => {
+      console.log(`Server is listening on PORT ${PORT}`);
+    });
+});
+
+export default app;
